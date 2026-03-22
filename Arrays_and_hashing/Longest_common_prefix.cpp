d3e047419266729ef4462143e3202a7404be5c9c
Dhushyanthc <dhushyanthc005@gmail.com>
2026-03-22 19:48:24 +0530

    brute force approach

---
+++ Arrays_and_hashing/Longest_common_prefix.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];  
+
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0;
+            
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+                j++;
+            }
+            prefix = prefix.substr(0, j);  
+
+            if (prefix.empty()) return ""; 
+        }
+
+        return prefix;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of strings: ";
+    cin >> n;
+
+    vector<string> strs(n);
+    cout << "Enter the strings: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> strs[i];
+    }
+
+    Solution sol;
+    cout << "Longest Common Prefix: " << sol.longestCommonPrefix(strs) << endl;
+
+    return 0;
+}

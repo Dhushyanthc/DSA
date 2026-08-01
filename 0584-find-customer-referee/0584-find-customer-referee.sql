64ca50a0c7b51807b14eac8bb5de83be243f2a7e
Dhushyanthc <dhushyanthc005@gmail.com>
2026-08-01 15:23:57 +0530

    Time: 699 ms (8.05%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,5 @@
-SELECT name FROM Customer WHERE referee_id IS NULL
-UNION ALL
-SELECT name FROM Customer WHERE referee_id < 2
-UNION ALL
-SELECT name FROM Customer WHERE referee_id > 2;
+SELECT name
+FROM Customer
+WHERE referee_id < 2 
+   OR referee_id > 2 
+   OR referee_id IS NULL;

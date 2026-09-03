a66a2a35035c0a6cf74000f12a625d4eb1fa8f9c
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-03 21:51:10 +0530

    Time: 0 ms (100%), Space: 11.2 MB (83.25%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,23 +2,12 @@ class Solution {
 public:
     bool check(vector<int>& a) {
         int n = a.size();
-        if (n == 0 || n ==1) return true;
-        int smallest = a[0];
-        bool turn = false;
-        for (int i = 1; i < n; i++){
-            if (a[i] >= a[i-1])
-            {
-                if ( turn == true && a[i] > a[0]){
-                   return false;
-                }
-                continue;
-            }else if (a[i] <= smallest && turn == false ){
-                turn = true;
-                smallest = a[i];
-            }else{
-                return false;
+        int count = 0;
+        for (int i = 0; i < n; i++){
+            if ( a[i] > a[(i+1)%n]){
+                count++;
             }
         }
-        return true;
+        return count <= 1;
     }
 };

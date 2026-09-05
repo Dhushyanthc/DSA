59b797ac1c735f38e14902598ecbc9298443f972
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-05 19:24:58 +0530

    Time: 6 ms (69.92%), Space: 26 MB (22.57%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (target > sum || target < -sum) {
+            return 0;
+        }
+
+        vector<vector<int>> dp(
+            n + 1,
+            vector<int>(2 * sum + 1, 0)
+        );
+
+        // Actual sum = 0
+        // Shifted index = sum
+        dp[0][sum] = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j <= 2 * sum; j++) {
+
+                if (dp[i - 1][j] != 0) {
+
+                    // Put +
+                    if (j + nums[i - 1] <= 2 * sum) {
+                        dp[i][j + nums[i - 1]]
+                            += dp[i - 1][j];
+                    }
+
+                    // Put -
+                    if (j - nums[i - 1] >= 0) {
+                        dp[i][j - nums[i - 1]]
+                            += dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[n][target + sum];
+    }
+};

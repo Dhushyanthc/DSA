e923bd7faf0e8ae3a29815ddb574fddcdabddd4b
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-13 00:05:23 +0530

    Time: 51 ms (76.48%), Space: 75.9 MB (65.29%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -11,25 +11,20 @@ public:
     int fun(string& s, int i, int j,
             vector<vector<int>>& dp) {
 
-        // No characters
         if (i > j)
             return 0;
 
-        // One character
         if (i == j)
             return 1;
 
-        // Already solved
         if (dp[i][j] != -1)
             return dp[i][j];
-
-        // Both ends match
+      
         if (s[i] == s[j]) {
             return dp[i][j] =
                 2 + fun(s, i + 1, j - 1, dp);
         }
-
-        // Ends don't match
+    
         return dp[i][j] =
             max(
                 fun(s, i + 1, j, dp),

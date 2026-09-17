3e27605cf7dd1ac97ec2fa58c0195274c0ebbe1f
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-17 14:31:30 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n);
-        for(int i = 0; i < n; i++){
-            vector<int> t(2,-1);
-            dp[i] = t;
-        }
-        return fun(nums, n, 0, 1, dp);
+        vector<int> dp(n, -1);
+        return solve(0, nums, n, dp);
     }
 
-    int fun(vector<int>& a, int n, int i, int free, vector<vector<int>>& dp){
-        if (i == n) return 0;
-        if(dp[i][free] != -1) return dp[i][free];
-        if ( free == 0 ) return dp[i][free] = fun(a, n, i+1, 1, dp);
-        int c1 = a[i] + fun(a, n, i+1, 0, dp);
-        int c2 = fun(a, n , i+1, 1, dp);
-        return dp[i][free] = max(c1,c2);
+    int solve(int i, vector<int>& nums, int n, vector<int>& dp) {
+        if(i >= n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int c1 = solve(i + 1, nums, n, dp);
+        int c2 = nums[i] + solve(i + 2, nums, n, dp);
+
+        return dp[i] = max(c1, c2);
     }
 };

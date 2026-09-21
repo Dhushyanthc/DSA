732624d5e495529ba5b34b374a3f65bd07143963
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-21 15:44:05 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.14%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return solve(root) != -1;
+    }
+
+    int solve(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+
+        int left = solve(root->left);
+        if(left == -1)
+            return -1;
+
+        int right = solve(root->right);
+        if(right == -1)
+            return -1;
+
+        if(abs(left - right) > 1)
+            return -1;
+
+        return 1 + max(left, right);
+    }
+};

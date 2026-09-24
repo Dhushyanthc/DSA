b1510ad4f5e2a837ed98e9bcdc93f9e6651995ae
Dhushyanthc <dhushyanthc005@gmail.com>
2026-09-24 20:54:14 +0530

    Time: 55 ms (92.37%), Space: 43.9 MB (78.54%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -4,35 +4,34 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-
-    TreeNode* ans = NULL;
+    TreeNode* ans = nullptr;
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        find(root, p, q);
+        int val = solve(root, p,q);
         return ans;
     }
 
-    int find(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL)
-            return 0;
+    int solve(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == nullptr) return 0;
 
-        int left = find(root->left, p, q);
-        int right = find(root->right, p, q);
+        int l = solve(root->left, p, q);
+        int r = solve(root->right, p, q);
 
         int self = 0;
+        if(root == p || root == q) self = 1;
 
-        if (root == p || root == q)
-            self = 1;
-
-        int total = self + left + right;
-
-        if (total == 2 && ans == NULL)
-            ans = root;
+        int total = self + l + r;
+        
+        if(total == 2 && ans == nullptr) ans = root;
 
         return total;
     }
+
 };
